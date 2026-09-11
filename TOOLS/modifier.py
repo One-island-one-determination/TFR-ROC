4842eab65317e3dab67f6180fcaed8159c20c0b7
gangang0508 <145277918+gangang0508@users.noreply.github.com>
2026-09-11 16:10:56 +0800

    fix several bugs from error log

--- TOOLS/modifier.py
+++ TOOLS/modifier.py
@@ -1,11 +1,9 @@
 input_text = """
-			consumer_goods_factor = -0.15
-
-			industrial_capacity_factor = 0.1
-
-			production_speed_rail_way_factor = 0.1
-
-			production_speed_infrastructure_factor = 0.1	
+			foreign_minister_cost_factor = -0.05
+				economic_minister_cost_factor = -0.05
+				interior_minister_cost_factor = -0.05
+				intelligence_minister_cost_factor = -0.05
+				theorist_minister_cost_factor = -0.05
 """
 
 # 將每一行拆解並轉換成目標格式
@@ -15,5 +13,5 @@ for line in input_text.strip().split('\n'):
         var_name, value = [item.strip() for item in line.split('=')]
         
         # 組合並印出 HOI4 腳本格式
-        output_line = f"add_to_variable = {{ CHI_BF_{var_name} = {value} tooltip = {var_name}_tooltip }}"
+        output_line = f"add_to_variable = {{ CHI_D_{var_name} = {value} tooltip = {var_name}_tooltip }}"
         print(output_line)
